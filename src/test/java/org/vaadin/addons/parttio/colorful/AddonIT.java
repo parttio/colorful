d7e113c497f871cacb0fcaf7adfbfede68b174ab
Matti Tahvonen <matti@vaadin.com>
2023-10-13 12:41:15 +0300

    Fixed IT, preparing for release

--- src/test/java/org/vaadin/addons/parttio/colorful/AddonIT.java
+++ src/test/java/org/vaadin/addons/parttio/colorful/AddonIT.java
@@ -12,23 +12,19 @@ import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertTha
 public class AddonIT {
 
     static Playwright playwright = Playwright.create(); // <4>
-    @Test
-    public void testClicking() {
-    }
-
 
     @Test
     public void addonTextIsRendered() {
         Browser browser = playwright.chromium().launch(); // <5>
         Page page = browser.newPage();
-        page.navigate("http://localhost:" + 8080 + "/");
-        page.getByText("Show value").click();
+        page.navigate("http://localhost:" + 8080 + "/" + HexColorPickerView.class.getSimpleName().replaceAll("View","").toLowerCase());
+        page.getByText("Show value").first().click();
 
         final String originalValue ="#ff0d00";
 
-        assertThat(page.locator("//p")).containsText(originalValue); // <8>
+        assertThat(page.locator("//p").first()).containsText(originalValue); // <8>
 
-        BoundingBox boundingBox = page.locator("//hex-color-picker").boundingBox();
+        BoundingBox boundingBox = page.locator("//hex-color-picker").first().boundingBox();
 
         var x = boundingBox.x + boundingBox.width/2;
         var y  = boundingBox.y + boundingBox.height/2;
@@ -36,9 +32,9 @@ public class AddonIT {
         // click in the middle and see that the value changes
         page.mouse().click(x, y);
 
-        page.getByText("Show value").click();
+        page.getByText("Show value").first().click();
 
-        assertThat(page.locator("//p")).not().containsText(originalValue);
+        assertThat(page.locator("//p").first()).not().containsText(originalValue);
 
     }
 }
